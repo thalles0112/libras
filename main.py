7cc18fa8297c1c690ad49f8eadfbc0f9d902a850
Thalles Oliveira <86566380+thalles0112@users.noreply.github.com>
2022-05-09 21:20:41 -0300

    1.0
    legendas dinamicas funcionando na gui, falta fazer o design

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
+from kivy.uix.label import Label
 import cv2 as cv
 import numpy as np
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.image import Image
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics.texture import Texture
-from tensorflow.keras.models import load_model
-
+from keras.models import load_model
+from kivy.lang import Builder
 
 model = load_model('modelconv2d-3')
-cap = cv.VideoCapture(0)
 
-predFinal = ''
 
 class OpenCam(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    img1= Image()
+    mytext = Label(text='aa')
+    camIndex = 0
+    def build(self):
+        Clock.schedule_interval(self.pred, 1.0/30.0)
         
-    def pred(self, dt):
+        box = BoxLayout()
+        box.orientation = 'vertical'
+        label = self.mytext
+        img = self.img1
+        box.add_widget(img)
+        box.add_widget(label)
+        return box
+ 
+    def pred(self, *args):
+        cap = cv.VideoCapture(self.camIndex)
         labels = ['oi', 'boa', 'tarde']
         ret, frame = cap.read()
         frame_ = cv.resize(frame, (300,300))
-        gray = cv.cvtColor(frame_, cv.COLOR_BGR2GRAY)
-        gray= cv.GaussianBlur(gray, (3, 3),0)
-        gray = cv.Canny(gray, 110, 135)
-        sobelx = cv.Sobel(gray, cv.CV_64F, 1,0)
-        sobely = cv.Sobel(gray, cv.CV_64F, 0,1)
-        combined_ = cv.bitwise_or(sobelx, sobely)
-        combined_p = np.array(combined_, 'float32')
-        combined_p = np.expand_dims(combined_p, 0)
-        combined_p= np.expand_dims(combined_p, -1)
+        frame_ = np.array(frame_, 'float32')
+        frame_ = np.expand_dims(frame_, 0)
+        frame_ = np.expand_dims(frame_, -1)
 
-        pred = np.argmax(model.predict(combined_p))
+        pred = np.argmax(model.predict(frame_))
 
         buf1 = cv.flip(frame, 0)   #inverte para não ficar de cabeça para baixo
         buf = buf1.tostring() # 
@@ -41,22 +45,14 @@ class OpenCam(App):
         texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             
         self.img1.texture = texture1
-        self.predFinal = labels[pred]
+        self.mytext.text = (labels[pred]).__str__()
+    def changeCam(self):
+        if self.camIndex == 0:
+            self.camIndex = 1
+        elif self.camIndex == 1:
+            self.camIndex = 0
         
-    def build(self):
-        self.img1 = Image(source='logoCL.jpg')
-       
-        label2= Label(text='teste')
-        layout = BoxLayout(orientation='vertical')  
-        
-        layout.add_widget(self.img1)
-        layout.add_widget(label2)
-         
-        self.capture = cv.VideoCapture(0)
-        ret, frame = self.capture.read() 
-        Clock.schedule_interval(self.pred, 1.0/30.0)
-        
-        return layout
-   
+
+
 if __name__ == '__main__':
     OpenCam().run()
